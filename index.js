6345182d3c9f61c917a66e86562599003dd0bbd6
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-08-26 12:05:32 -0600

    Easy corners are mostly working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let game = []
 const difficulty = document.querySelector('select')
+const remaining = document.querySelector('.mines-count-hold')
+let mines = null
 
 const newGame = () => {
   game = []
@@ -7,6 +9,8 @@ const newGame = () => {
   playArea.innerHTML = ''
   playArea.classList = ''
   if (difficulty.value === 'easy') {
+    mines = 10
+    remaining.innerText = mines
     playArea.classList.add('easy')
     getSquares(81)
 
@@ -18,6 +22,8 @@ const newGame = () => {
       playArea.append(element)
     })
   } else if (difficulty.value === 'medium') {
+    mines = 40
+    remaining.innerText = mines
     playArea.classList.add('medium')
     getSquares(256)
 
@@ -27,6 +33,8 @@ const newGame = () => {
       playArea.append(element)
     })
   } else if (difficulty.value === 'hard') {
+    mines = 99
+    remaining.innerText = mines
     playArea.classList.add('hard')
     getSquares(480)
 
@@ -44,10 +52,24 @@ const getSquares = (count) => {
     let newSquare = document.createElement('div')
     newSquare.classList.add('game-square')
     newSquare.id = id
+    newSquare.innerText = id
     newSquare.addEventListener('auxclick', (e) => {
+      mines--
+      remaining.innerText = mines
       newSquare.classList.add('flag-square')
       newSquare.removeEventListener('click', lose)
       newSquare.innerHTML = `<img class='flag' src='./Assets/flag.png' />`
+      if (mines === 0) {
+        let mineSquares = document.getElementsByClassName('mine-square')
+        let flagSquares = document.getElementsByClassName('flag-square')
+        for (let i = 0; i < mineSquares.length; i++) {
+          if (+mineSquares[i].id !== +flagSquares[i].id) {
+            alert('Wrong')
+            break
+          }
+        }
+        alert('You win')
+      }
     })
     newSquare.addEventListener('click', (e) => {
       click(+e.srcElement.id)
@@ -72,66 +94,144 @@ const getMines = (numMines, length) => {
     let square = game[element]
     square.addEventListener('click', lose)
     square.classList.add('mine-square')
-    // square.innerHTML = `<img class='mine' src='./Assets/mine.ico' />`
+    square.innerHTML = `<img class='mine' src='./Assets/mine.ico' />`
   })
 
   return minesIndex
 }
 
 const click = (id, callingSquare) => {
-  console.log(`ID: ${id}`)
+  // console.log(`ID: ${id}`)
   let num = 0
   let square = document.getElementById(id)
   square.classList.add('clicked')
-  let squareChecks = []
-  const corners = []
   if (difficulty.value === 'easy') {
-    squareChecks.push(game[id - 1], game[id + 1], game[id - 9], game[id + 9])
-    corners.push(game[id - 10], game[id - 8], game[id + 8], game[id + 10])
-  }
-  if (difficulty.value === 'medium') {
-    squareChecks.push(game[id - 1], game[id + 1], game[id - 16], game[id + 16])
-    corners.push(game[id - 17], game[id - 15], game[id + 15], game[id + 17])
-  }
-  if (difficulty.value === 'hard') {
-    squareChecks.push(game[id - 1], game[id + 1], game[id - 30], game[id + 30])
-    corners.push(game[id - 31], game[id -29], game[id + 29], game[id + 31])
-  }
 
-  corners.forEach(element => {
-    if(element.classList.contains('mine-square')){
-      num++
+    //DECIDES WHICH SQUARES TO LOOK AT ON EASY
+
+    let checks = getChecks('easy', id)
+
+    //DECIDES WHICH SQUARES TO LOOK AT ON MEDIUM
+
+    if (difficulty.value === 'medium') {
+      const mediumLeft = [0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240]
+      const mediumRight = [15, 31, 47, 63, 79, 95, 111, 127, 143, 159, 175, 191, 207, 223, 239, 255]
+      if (mediumLeft.includes(+id)) {
+        squareChecks.push(game[id + 1], game[id - 16], game[id + 16])
+        corners.push(game[id + 15], game[id + 17])
+      } else if (mediumRight.includes(+id)) {
+        squareChecks.push(game[id - 1], game[id - 16], game[id + 16])
+        corners.push(game[id + 15], game[id + 17])
+      } else {
+        squareChecks.push(game[id - 1], game[id + 1], game[id - 16], game[id + 16])
+        corners.push(game[id - 17], game[id - 15], game[id + 15], game[id + 17])
+      }
     }
-  })
 
-  squareChecks.forEach(element => {
-    if (element && element.classList.contains('mine-square')) {
-      num++
+    //DECIDES WHICH SQUARES TO LOOK AT ON HARD
+
+    if (difficulty.value === 'hard') {
+      const hardLeft = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450]
+      const hardRight = [29, 59, 89, 119, 149, 179, 209, 239, 269, 299, 329, 359, 389, 419, 449, 479]
+      if (hardLeft.includes(+id)) {
+        squareChecks.push(game[id + 1], game[id - 30], game[id + 30])
+        corners.push(game[id + 29], game[id + 31])
+      } else if (hardRight.includes(+id)) {
+        squareChecks.push(game[id - 1], game[id - 30], game[id + 30])
+        corners.push(game[id - 31], game[id - 29])
+      } else {
+        squareChecks.push(game[id - 1], game[id + 1], game[id - 30], game[id + 30])
+        corners.push(game[id - 31], game[id - 29], game[id + 29], game[id + 31])
+      }
     }
-  })
 
-  squareChecks.forEach(element => {
-    if (num === 0 && element && !element.classList.contains('clicked')) {
-      click(+element.id, +id)
-    }
-  })
+    checks.corners.forEach(element => {
+      if (element && element.classList.contains('mine-square')) {
+        num++
+      }
+    })
 
-  if (num !== 0) {
-    square.innerText = num
+    checks.squareChecks.forEach(element => {
+      if (element && element.classList.contains('mine-square')) {
+        num++
+      }
+    })
+
+    // checks.squareChecks.forEach(element => {
+    //   if (num === 0 && element && !element.classList.contains('clicked')) {
+    //     click(+element.id, +id)
+    //   }
+    // })
+
+    // if (num !== 0) {
+    //   square.innerText = num
+    // }
+    return num
   }
-  return num
 }
 
-const lose = () => {
-  alert('lose')
-  game = []
+  const getChecks = (difficulty, id) => {
+    let squareChecks = []
+    const corners = []
+    switch (difficulty) {
+      case 'easy':
+        const easyLeft = [9, 18, 27, 36, 45, 54, 63]
+        const easyRight = [17, 26, 35, 44, 53, 62, 71]
+        const easyTop = [1, 2, 3, 4, 5, 6, 7]
+        const easyBottom = [73, 74, 75, 76, 77, 78, 79]
+
+        switch (+id) {
+          case 0:
+            corners.push(game[10])
+            squareChecks.push(game[1], game[9])
+            break;
+          case 72:
+            corners.push(game[64])
+            squareChecks.push(game[63], game[73])
+            break;
+          case 8:
+            corners.push(game[16])
+            squareChecks.push(game[7], game[17])
+            break;
+          case 80:
+            corners.push(game[70])
+            squareChecks.push(game[71], game[79])
+            break;
+          default:
+            if (easyLeft.includes(+id)) {
+              squareChecks.push(game[id + 1], game[id - 9], game[id + 9])
+
+            } else if (easyRight.includes(+id)) {
+              squareChecks.push(game[id - 1], game[id - 9], game[id + 9])
+
+            } else {
+              squareChecks.push(game[id - 1], game[id + 1], game[id - 9], game[id + 9])
+              corners.push(game[id - 10], game[id - 8], game[id + 8], game[id + 10])
+            }
+        }
+        console.log(corners)
+        break;
+      case 'medium':
+        break
+      case 'hard':
+        break
+      default:
+        return 'error'
+    }
+
+    return {
+      squareChecks,
+      corners
+    }
+  }
+
+  const lose = () => {
+    alert('lose')
+    game = []
+    newGame()
+  }
+
+
   newGame()
-}
-
-
-newGame()
-
-
-
 
 
